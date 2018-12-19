3d98bffffb5b7fd20ea0f97f503d1a04af990b8b
ScottWorks <scottworks.io@gmail.com>
2018-12-18 22:31:53 -0700

    moved logic to helper functions

--- utils/files.js
+++ utils/files.js
@@ -7,17 +7,15 @@ const { KATA_REPO_PATH } = process.env;
 module.exports = (katas) => {
   katas.forEach((kata) => {
     const { kataLevel, kataTitle, kataLink, kataCode } = kata,
-      checkFolderExists = fs.existsSync(`${KATA_REPO_PATH}/${kataLevel}`);
+      folderPath = `${KATA_REPO_PATH}/${kataLevel}`,
+      folderExists = checkFolderExists(folderPath);
 
-    if (!checkFolderExists) {
+    if (!folderExists) {
       console.log(`Creating directory for ${kataLevel}!`);
-      fs.mkdir(`${KATA_REPO_PATH}/${kataLevel}`, (err) => {
-        if (err) {
-          console.error(err);
-          throw err;
-        }
-        console.log(`A directory for ${kataLevel} has been created!`);
-      });
+      createDirectory(
+        folderPath,
+        `A directory for ${kataLevel} has been created!`
+      );
     }
 
     const payload = formatJSFile(`
@@ -27,16 +25,35 @@ module.exports = (katas) => {
     ${kataCode}
     `);
 
-    fs.writeFile(
-      `${KATA_REPO_PATH}/${kataLevel}/${kataTitle}.js`,
+    createFile(
+      `${folderPath}/${kataTitle}.js`,
       payload,
-      (err) => {
-        if (err) {
-          console.error(err);
-          throw err;
-        }
-        console.log(`${kataTitle}.js has been saved!`);
-      }
+      `${kataTitle}.js has been saved!`
     );
   });
 };
+
+// Helper Functions
+function checkFolderExists(path) {
+  return fs.existsSync(path);
+}
+
+function createDirectory(path, message) {
+  fs.mkdir(path, (err) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+    console.log(message);
+  });
+}
+
+function createFile(path, payload, message) {
+  fs.writeFile(path, payload, (err) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+    console.log(message);
+  });
+}
