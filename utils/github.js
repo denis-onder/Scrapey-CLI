9028d2c0661fa12dd691c7b66a726a9b6897b148
ScottWorks <scottworks.io@gmail.com>
2018-12-20 00:21:46 -0700

    git init and add remote are working

--- utils/github.js
+++ utils/github.js
@@ -1,10 +1,37 @@
 require('dotenv').config();
-const simpleGit = require('simple-git');
+const { DIR_PATH, GITHUB_REMOTE } = process.env,
+  git = require('simple-git/promise')(DIR_PATH),
+  files = require('./files');
 
 module.exports = {
-  // Git Init
-  // Git Add
-  // Git Commit
-  // Git Add Remote
-  // Git Push
+  initGit: function() {
+    git.checkIsRepo().then(function(isRepo) {
+      !isRepo && initializeRepo(git);
+    });
+  },
+  commitChanges: function() {
+    stageChanges();
+    // git commit
+  },
+  pushChanges: function() {
+    // git push
+  }
 };
+
+function initializeRepo(git) {
+  console.log('Initializing git repo and adding remote...');
+  return git
+    .init()
+    .then(function() {
+      console.log('Git repo initialized...');
+    })
+    .then(function() {
+      git.addRemote('origin', DIR_PATH, function() {
+        console.log('Remote added...');
+      });
+    });
+}
+
+function stageChanges() {
+  // git add
+}
