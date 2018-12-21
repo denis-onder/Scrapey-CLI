e979e33eaab84b9f1c756ac13aa0e1bb7232d9f5
ScottWorks <scottworks.io@gmail.com>
2018-12-20 23:21:29 -0700

    files and folders made synchronously :fire:

--- utils/files.js
+++ utils/files.js
@@ -5,30 +5,16 @@ const fs = require('fs'),
 const { DIR_PATH } = process.env;
 
 module.exports = {
-  // createReadmeFile: function(){
-  //     // Stage and Commit changes
-  //     // Push changes
-  // },
-  checkPath: function(path, dirName, kataTitle, payload, callback) {
-    const exists = fs.existsSync(path);
-    if (!exists) {
-      console.log(`Creating directory for ${dirName}!`);
-      createDirectory(
-        path,
-        `A directory for ${dirName} has been created!`,
-        kataTitle,
-        payload,
-        callback
-      );
-    } else {
-      callback(path, kataTitle, payload);
-    }
+  checkPath: function(path) {
+    return new Promise(function(resolve, reject) {
+      resolve(fs.existsSync(path));
+    });
   },
-  createJSFiles: function(katas) {
-    katas.forEach((kata) => {
+  createJSFiles: async function(katas) {
+    for (let kata of katas) {
       const { kataLevel, kataTitle, kataLink, kataCode } = kata;
 
-      const folderPath = `${DIR_PATH}/${kataLevel}`;
+      const path = `${DIR_PATH}/${kataLevel}`;
 
       const payload = formatJSFile(`
         // ${kataTitle}
@@ -37,40 +23,52 @@ module.exports = {
         ${kataCode}
         `);
 
-      this.checkPath(folderPath, kataLevel, kataTitle, payload, function(
-        folderPath,
-        kataTitle,
-        payload
-      ) {
-        createFile(
-          `${folderPath}/${kataTitle}.js`,
-          payload,
-          `${kataTitle}.js has been saved!`
-        );
+      await this.checkPath(path).then(function(exists) {
+        if (!exists) {
+          createDirectory(path)
+            .then(function() {
+              console.log(`Added a directory for level ${kataLevel} katas!\n`);
+            })
+            .then(function() {
+              createFile(`${path}/${kataTitle}.js`, payload);
+            })
+            .then(function() {
+              console.log(`${kataTitle}.js has been saved!\n`);
+            });
+        } else {
+          createFile(`${path}/${kataTitle}.js`, payload).then(function() {
+            console.log(`${kataTitle}.js has been saved!`);
+          });
+        }
       });
       // Stage and Commit changes
-    });
-    // Push changes
+      // Push changes
+    }
   }
 };
 
-function createDirectory(path, message, kataTitle, payload, callback) {
-  fs.mkdirSync(path, (err) => {
-    if (err) {
-      console.error(err);
-      throw err;
-    }
-    console.log(message);
-    callback(path, kataTitle, payload);
+function createDirectory(path) {
+  return new Promise(function(resolve, reject) {
+    resolve(
+      fs.mkdirSync(path, (err) => {
+        if (err) {
+          console.error(err);
+          throw err;
+        }
+      })
+    );
   });
 }
 
-function createFile(path, payload, message) {
-  fs.writeFileSync(path, payload, (err) => {
-    if (err) {
-      console.error(err);
-      throw err;
-    }
-    console.log(message);
+function createFile(path, payload) {
+  return new Promise(function(resolve, reject) {
+    resolve(
+      fs.writeFileSync(path, payload, (err) => {
+        if (err) {
+          console.error(err);
+          throw err;
+        }
+      })
+    );
   });
 }
